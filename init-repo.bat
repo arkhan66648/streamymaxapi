d9520880c38ddcfb2d4b7ecfdd2553b83911c3e6
arkhan66648 <rmadskhanfirst4323@gmail.com>
2026-07-25 00:43:50 +0600

    Fresh start: unmodified streamymax.com clone + robots.txt [skip ci]

--- init-repo.bat
+++
@@ -1,6 +0,0 @@
-git init
-git remote add origin https://github.com/arkhan66648/streamymaxapi.git
-git add -A
-git commit -m "Initial commit: streamymax api parser and github actions"
-git branch -M main
-git push -u origin main --force
